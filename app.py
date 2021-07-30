642aeecd1926760e3aafaf8f13d3ec56b996951d
TobiasOyaniran <tobiasoyaniran@gmail.com>
2021-07-30 17:41:26 +0000

    all changes

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ from flask import request
 app = Flask(__name__)
 
 # -- Routes section --
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/choose_activity")
+def choose_activity():
+    if 1:
+        print("We recommend browsing through some memes!")
+    return "Choose an activity!"
